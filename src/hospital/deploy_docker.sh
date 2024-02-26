ea595a8c447bfb907a9d965b8b91d144ac776939
BRAHIM Abderrazak <brahim.abderrazak1307@gmail.com>
2024-02-26 22:28:08 +0100

    Tested auth inside kubernetes cluster. Split Kong routing into multiple files

--- src/hospital/deploy_docker.sh
+++ src/hospital/deploy_docker.sh
@@ -16,8 +16,8 @@ do
     printf '** Service %-25s:' "$service_name" 
     docker build -t $service_name ./$service &> /dev/null
     printf ' %s' "built"
-    docker tag $service_name $REGISTRY/$service_name &> /dev/null
-    docker push $REGISTRY/$service_name &> /dev/null
+    docker tag $service_name $REGISTRY/$service_name:latest &> /dev/null
+    docker push $REGISTRY/$service_name:latest &> /dev/null
     printf ', %s\n' "pushed!"
 done
 
