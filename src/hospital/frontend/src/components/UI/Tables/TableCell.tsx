f035ae7a54461290111188facc68035fb9d98e11
BRAHIM Abderrazak <brahim.abderrazak1307@gmail.com>
2024-03-08 09:49:08 +0100

    Fixed some unfinished merges

--- src/hospital/frontend/src/components/UI/Tables/TableCell.tsx
+++ src/hospital/frontend/src/components/UI/Tables/TableCell.tsx
@@ -5,17 +5,8 @@ type TableCellProps = {
   className?: string;
 };
 
-<<<<<<< HEAD
 function TableCell({ children, className }: TableCellProps) {
   return <td className={`text-left ${className}`}>{children}</td>;
-=======
-function TableCell({ children, className='' }: TableCellProps) {
-  return (
-      <td className={`text-left ${className}`} >
-        {children}
-      </td>
-  )
->>>>>>> f16d90fbefbfa0bb71326482f6700b5e2820d787
 }
 
 export default TableCell;
