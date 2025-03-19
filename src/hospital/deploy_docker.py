0aab5025484b361a48135395b22af18d680ac1dc
LemonLamo <94323231+LemonLamo@users.noreply.github.com>
2025-03-19 17:20:28 +0100

    what can i say besides you're welcome

--- src/hospital/deploy_docker.py
+++ src/hospital/deploy_docker.py
@@ -21,9 +21,14 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 
 for service in services:
     service_name = service.split("/")[-1]
-    print(f"** Service {service_name.ljust(25)}:")
-    subprocess.run([f"docker build -t {service_name} ./{service}"], shell=True, text=True, cwd=cwd)
-    subprocess.run([f"docker tag {service_name} {registry}/{service_name}"], shell=True, text=True, cwd=cwd)
-    subprocess.run([f"docker push {registry}/{service_name}"], shell=True, text=True, cwd=cwd)
+    print(f"** Service {service_name.ljust(25)}:", end=" ")
+    s = subprocess.Popen(f"docker build -t {service_name} ./{service}", shell=True, cwd=cwd)
+    s.wait()
+    print("built", end=", ")
+    s = subprocess.Popen(f"docker tag {service_name} {registry}/{service_name}", shell=True, text=True, cwd=cwd)
+    s.wait()
+    s = subprocess.Popen(f"docker push {registry}/{service_name}", shell=True, text=True, cwd=cwd)
+    s.wait()
+    print("pushed!")
 
 print("All services built and pushed!")
